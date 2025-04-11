822cac6da60359b0a0ae717fd9e05cf4fab83a16
Chirag Jain <chirag8966@gmail.com>
2025-04-11 20:18:02 +0530

    feat: create common inputs

---
+++ src/app/components/common/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() label: string = 'Submit';
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+}
